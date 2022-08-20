bca839dc91dda88b2745b527369d961f4053fc22
MadLab <44909045+MadLabDesign@users.noreply.github.com>
2022-08-20 02:15:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mld-auth-page
+
+React App with Typescript and AWS Amplify to add Sign up, Login and Logout
---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '@aws-amplify/ui-react/styles.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
+import styled from 'styled-components/macro';
+Amplify.configure(awsconfig);
+function App() {
+  return (
+    <Authenticator>
+    {({ signOut, user }) => (
+      <Main>
+        <h1>Hello {user?.username}</h1>
+        <button onClick={signOut}>Sign out</button>
+      </Main>
+    )}
+  </Authenticator>
+  );
+}
+
+export default withAuthenticator(App);
+
+const Main = styled.main`
+background-color: red ;
+`
